4717a32171b3799fc5dcaf845d33bf979ed6da5e
Hernán Valencia <hernanadrianv17@gmail.com>
2025-07-04 18:48:08 -0500

    🚧 wip: setting up factor calculator with tabs component and preparing everything to add interest calculator forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+import FactorCalculator from "./features/FactorCalculator";
 
 function App() {
-  
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <header className="my-10 text-center">
         <h1 className="text-4xl font-bold">Calculadora Económica</h1>
       </header>
-   <Tabs defaultValue="account" className="w-[400px]">
-  <TabsList>
-    <TabsTrigger value="account">Account</TabsTrigger>
-    <TabsTrigger value="password">Password</TabsTrigger>
-  </TabsList>
-  <TabsContent value="account">Make changes to your account here.</TabsContent>
-  <TabsContent value="password">Change your password here.</TabsContent>
-</Tabs>
+      <Tabs defaultValue="factor" className="w-full max-w-3xl mx-auto">
+        <TabsList>
+          <TabsTrigger value="factor">Calculadora de Factor</TabsTrigger>
+          <TabsTrigger value="interes">Interés</TabsTrigger>
+        </TabsList>
+        <TabsContent value="factor">
+          <FactorCalculator />
+        </TabsContent>
+        <TabsContent value="interes">Change your password here.</TabsContent>
+      </Tabs>
     </ThemeProvider>
   );
 }
